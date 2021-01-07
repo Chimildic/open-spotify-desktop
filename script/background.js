edc0ca19a6e3a619bd9e571faee02ba259b7f1dc
Chimildic <40138097+Chimildic@users.noreply.github.com>
2021-01-07 21:45:58 +0300

    simplify create url

--- script/background.js
+++ script/background.js
@@ -3,28 +3,16 @@ let extraInfoSpec = ['blocking'];
 chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest, filter, extraInfoSpec);
 
 function onBeforeRequest(details) {
-    let modifiedUrl = createUrlToDesktopApp(details.url) || '';
+    let modifiedUrl = createUrlToDesktopApp(details.url);
     if (modifiedUrl.length > 0) {
         closeSelectedTab(details.url, () => openNewTab(modifiedUrl));
     }
 }
 
 function createUrlToDesktopApp(urlSource) {
-    if (isValidUrlSource()) {
-        return parse();
-    }
-
-    function isValidUrlSource() {
-        return !['/embed', '/log'].some((str) => urlSource.includes(str));
-    }
-
-    function parse() {
-        let pattern = '\/(track|playlist|album|artist|show|episode|user)\/([^\#\/\?]+)';
-        let groups = urlSource.match(new RegExp(pattern, 'i'));
-        if (groups != null && groups.length == 3) {
-            return `/page/redirect.html?url=spotify:${groups[1]}:${groups[2]}`;
-        }
-    }
+    let pattern = 'open.spotify.com\/(track|playlist|album|artist|show|episode|concert|user)\/([^\?\/\#\& ]+)';
+    let [fullMatch, type, id] = urlSource.match(new RegExp(pattern, 'i')) || [];
+    return fullMatch ? `/page/redirect.html?url=spotify:${type}:${id}` : '';
 }
 
 function openNewTab(url) {
