c2ec0addb35466a901113c67f11b17a26a20a680
Chimildic <40138097+Chimildic@users.noreply.github.com>
2021-01-07 22:27:11 +0300

    close tab without check url

--- script/background.js
+++ script/background.js
@@ -5,12 +5,12 @@ chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest, filter, extraInfo
 function onBeforeRequest(details) {
     let modifiedUrl = createUrlToDesktopApp(details.url);
     if (modifiedUrl.length > 0) {
-        closeSelectedTab(details.url, () => openNewTab(modifiedUrl));
+        closeSelectedTab(() => openNewTab(modifiedUrl));
     }
 }
 
 function createUrlToDesktopApp(urlSource) {
-    let pattern = 'open.spotify.com\/(track|playlist|album|artist|show|episode|concert|user)\/([^\?\/\#\& ]+)';
+    let pattern = 'open.spotify.com/(track|playlist|album|artist|show|episode|concert|user)/([^?/#& ]+)';
     let [fullMatch, type, id] = urlSource.match(new RegExp(pattern, 'i')) || [];
     return fullMatch ? `/page/redirect.html?url=spotify:${type}:${id}` : '';
 }
@@ -19,9 +19,9 @@ function openNewTab(url) {
     chrome.tabs.create({ url: url });
 }
 
-function closeSelectedTab(url, callback) {
+function closeSelectedTab(callback) {
     chrome.tabs.getSelected((tab) => {
-        if (typeof tab == 'object' && tab.id > 0 && (tab.pendingUrl === url || tab.url === url)) {
+        if (typeof tab == 'object' && tab.id > 0) {
             chrome.tabs.remove(tab.id, callback);
         }
     });
