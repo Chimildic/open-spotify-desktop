7653ecb3c13fc722f47179e2381a537227203ce0
Chimildic <40138097+Chimildic@users.noreply.github.com>
2021-01-24 15:33:46 +0300

    refactor feedback

--- script/feedback.js
+++ script/feedback.js
@@ -1,10 +1,11 @@
 const WEBSTORE_PAGE = `https://chrome.google.com/webstore/detail/${chrome.runtime.id}`;
 document.addEventListener('DOMContentLoaded', onLoadedPage);
+setOptions({ lastShownFeedback: new Date().toUTCString() });
 
 function onLoadedPage() {
     setLocale();
-    setShareLink();
-    onClickWebStorePage.addEventListener('click', showWebStorePage);
+    setHrefShare();
+    elWebStorePage.addEventListener('click', onClickWebStorePage);
 }
 
 function setLocale() {
@@ -12,17 +13,19 @@ function setLocale() {
     elements.forEach((el) => (el.innerHTML = chrome.i18n.getMessage(el.dataset.locale)));
 }
 
-function setShareLink(){
-    hrefTwitter.href = `http://twitter.com/share?text=${chrome.i18n.getMessage('share_twitter')}&url=${WEBSTORE_PAGE}`;
+function setHrefShare() {
+    let twitterText = chrome.i18n.getMessage('share_twitter');
+    elTwitter.href = `https://twitter.com/share?text=${twitterText}&url=${WEBSTORE_PAGE}`;
 }
 
-function showWebStorePage() {
+function onClickWebStorePage() {
     window.open(WEBSTORE_PAGE);
+    setOptions({ canShownFeedback: false });
+}
+
+function setOptions(content) {
     chrome.runtime.sendMessage({
         action: 'setOptions',
-        content: {
-            canShownFeedback: false,
-            lastShownFeedback: new Date().toUTCString(),
-        },
+        content: content,
     });
 }
